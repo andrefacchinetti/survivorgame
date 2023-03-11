0b890a5f3d7018c968bfe0d2c2b71d05c8f7dfdd
Andre Facchinetti <andrebe07@gmail.com>
2023-03-11 19:45:56 -0300

    vaca e peppa pig

--- Assets/Animal pack deluxe/Scripts/AnimalPassivo.cs
+++ Assets/Animal pack deluxe/Scripts/AnimalPassivo.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using Photon.Pun;
 
+[RequireComponent(typeof(PhotonView), typeof(NavMeshAgent))]
 public class AnimalPassivo : MonoBehaviourPunCallbacks
 {
 
