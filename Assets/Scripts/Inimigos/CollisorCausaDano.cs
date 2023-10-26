9511dbe0e40e5e617b49fbfb91dd0b0d5504971d
Andre Facchinetti <andrebe07@gmail.com>
2023-10-26 20:22:35 -0300

    att

--- Assets/Scripts/Inimigos/CollisorCausaDano.cs
+++ Assets/Scripts/Inimigos/CollisorCausaDano.cs
@@ -17,20 +17,23 @@ public class CollisorCausaDano : MonoBehaviourPunCallbacks
     {
         if (!statsGeral.isAttacking) return;
         CollisorSofreDano collisorSofreDano = other.gameObject.GetComponent<CollisorSofreDano>();
-        if (collisorSofreDano != null && collisorSofreDano.PV.ViewID != PV.ViewID)
+        if (collisorSofreDano != null && collisorSofreDano.PV.ViewID != PV.ViewID) //VERIFICA SE NAO ESTA BATENDO EM SI PROPRIO
         {
-            float damage = statsGeral.damage;
-            ItemObjMao itemObjMao = GetComponent<ItemObjMao>();
-            if (itemObjMao != null)
+            if (!(GetComponentInParent<LobisomemController>() != null && other.gameObject.GetComponentInParent<LobisomemController>() != null)) //Verificar que um lobo nao esta batendo em outro lobo
             {
-                damage = collisorSofreDano.CalcularDanoPorArmaCausandoDano(itemObjMao, statsGeral.damage);
-            }
-            GameObject objPai = collisorSofreDano.gameObject.GetComponentInParent<StatsGeral>().gameObject;
-            objPai.GetComponent<StatsGeral>().TakeDamage(damage);
-            statsGeral.isAttacking = false;
-            if (statsGeral.gameObject.tag == "Player")
-            {
-                statsGeral.gameObject.GetComponent<Animator>().SetTrigger("ferramentaFrenteExit");
+                float damage = statsGeral.damage;
+                ItemObjMao itemObjMao = GetComponent<ItemObjMao>();
+                if (itemObjMao != null)
+                {
+                    damage = collisorSofreDano.CalcularDanoPorArmaCausandoDano(itemObjMao, statsGeral.damage);
+                }
+                GameObject objPai = collisorSofreDano.gameObject.GetComponentInParent<StatsGeral>().gameObject;
+                objPai.GetComponent<StatsGeral>().TakeDamage(damage);
+                statsGeral.isAttacking = false;
+                if (statsGeral.gameObject.tag == "Player")
+                {
+                    statsGeral.gameObject.GetComponent<Animator>().SetTrigger("ferramentaFrenteExit");
+                }
             }
         }
     }
