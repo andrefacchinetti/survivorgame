2b6ccedf78af41daa23942088cc0b6f6df0b0e21
Andre Facchinetti <andrebe07@gmail.com>
2023-11-03 18:37:13 -0300

    dano ao Cair de lugar alto

--- Assets/Scripts/Jogador/Grab/CheckGroundedContact.cs
+++ Assets/Scripts/Jogador/Grab/CheckGroundedContact.cs
@@ -7,6 +7,41 @@ public class CheckGroundedContact : MonoBehaviourPunCallbacks
 {
 
     [SerializeField] GrabObjects grabObjects;
+    public CharacterController characterController;
+    public float heightBeforeFall = 3;
+    public float fallDamageMultiplier = 10f;
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = characterController.transform.position;
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            // Se o jogador estiver no chão, atualize a posição inicial
+            startPosition = characterController.transform.position;
+        }
+    }
+
+    void LateUpdate()
+    {
+        // Detecta a altura da queda quando o jogador toca o chão novamente
+        if (characterController.isGrounded)
+        {
+            float fallenHeight = startPosition.y - characterController.transform.position.y;
+
+            if (fallenHeight > heightBeforeFall)
+            {
+                int damage = Mathf.RoundToInt((fallenHeight - heightBeforeFall) * fallDamageMultiplier);
+
+                // Aplica dano ao jogador
+                characterController.GetComponent<StatsGeral>().TakeDamage(damage);
+            }
+        }
+    }
 
     void OnTriggerStay(Collider collision)
     {
