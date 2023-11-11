f8ff8235add53092784bb1ca73afefc9832691c8
Andre Facchinetti <andrebe07@gmail.com>
2023-11-11 16:35:27 -0300

    emotes F1 F2 F3 F4 F5 F6

--- Assets/Scripts/Jogador/EmotesController.cs
+++ Assets/Scripts/Jogador/EmotesController.cs
@@ -19,16 +19,36 @@ public class EmotesController : MonoBehaviour
     {
         if (!playerController.podeSeMexer()) return;
 
-        if (Input.GetKeyDown(KeyCode.F1)) animator.SetTrigger("animationF1");
-        else if (Input.GetKeyDown(KeyCode.F2)) animator.SetTrigger("animationF2");
-        else if (Input.GetKeyDown(KeyCode.F3)) animator.SetTrigger("animationF3");
-        else if (Input.GetKeyDown(KeyCode.F4)) animator.SetTrigger("animationF4");
-        else if (Input.GetKeyDown(KeyCode.F5)) animator.SetTrigger("animationF5");
-        else if (Input.GetKeyDown(KeyCode.F9))
+        if (Input.GetKeyDown(KeyCode.F1)) { desativarAnimacoesDeBraco("animationF1"); animator.SetBool("animationF1", !animator.GetBool("animationF1")); }
+        else if (Input.GetKeyDown(KeyCode.F2)) { desativarAnimacoesDeBraco("animationF2"); animator.SetBool("animationF2", !animator.GetBool("animationF2")); }
+        else if (Input.GetKeyDown(KeyCode.F3)) { desativarAnimacoesDePerna("animationF3"); animator.SetBool("animationF3", !animator.GetBool("animationF3")); }
+        else if (Input.GetKeyDown(KeyCode.F4)) { desativarAnimacoesDeBraco("animationF4"); animator.SetBool("animationF4", !animator.GetBool("animationF4")); }
+        else if (Input.GetKeyDown(KeyCode.F5)) { desativarAnimacoesDeBraco("animationF5"); animator.SetBool("animationF5", !animator.GetBool("animationF5")); }
+        else if (Input.GetKeyDown(KeyCode.F6)) { desativarAnimacoesDeBraco("animationF6"); animator.SetBool("animationF6", !animator.GetBool("animationF6")); }
+
+        if (!animator.GetBool("isPlayerParado"))
         {
-            Destroy(this.gameObject);
-            SceneManager.LoadScene(0);
+            desativarAnimacoesDePerna("");
         }
+        if (animator.GetBool("isPlayerArmado"))
+        {
+            desativarAnimacoesDeBraco("");
+        }
+        
+    }
+
+    private void desativarAnimacoesDePerna(string animacaoAtivando)
+    {
+        if (animacaoAtivando != "animationF3") animator.SetBool("animationF3", false);
+    }
+
+    private void desativarAnimacoesDeBraco(string animacaoAtivando)
+    {
+        if(animacaoAtivando != "animationF1") animator.SetBool("animationF1", false);
+        if (animacaoAtivando != "animationF2") animator.SetBool("animationF2", false);
+        if (animacaoAtivando != "animationF4") animator.SetBool("animationF4", false);
+        if (animacaoAtivando != "animationF5") animator.SetBool("animationF5", false);
+        if (animacaoAtivando != "animationF6") animator.SetBool("animationF6", false);
     }
 
 }
