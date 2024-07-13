85c8316d1e659491bc415f3bf164196dcfc4b497
Andre Facchinetti <andrebe07@gmail.com>
2024-07-13 17:02:43 -0300

    pacote hud

---
+++ Assets/RPG and MMO UI 11/Scripts/Demo/Demo_FixScrollRects.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+namespace DuloGames.UI
+{
+    public class Demo_FixScrollRects : MonoBehaviour
+    {
+
+        protected void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnLoaded;
+        }
+
+        protected void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnLoaded;
+        }
+
+        private void OnLoaded(Scene scene, LoadSceneMode mode)
+        {
+            ScrollRect[] rects = Component.FindObjectsOfType<ScrollRect>();
+
+            foreach (ScrollRect rect in rects)
+            {
+                LayoutRebuilder.MarkLayoutForRebuild(rect.viewport);
+            }
+        }
+    }
+}
