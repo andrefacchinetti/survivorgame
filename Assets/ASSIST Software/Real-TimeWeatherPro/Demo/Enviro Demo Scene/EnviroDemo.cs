f50a8b5fe8f9fdb9865a31273d162069afb995d2
Andre Facchinetti <andrebe07@gmail.com>
2024-07-15 17:18:20 -0300

    expanse HDRP RealTimeWeather

---
+++ Assets/ASSIST Software/Real-TimeWeatherPro/Demo/Enviro Demo Scene/EnviroDemo.cs
@@ -0,0 +1,32 @@
+﻿//
+// Copyright(c) 2023 Real-Time Weather ASSIST Software
+// https://assist-software.net
+//
+// For information about the licensing and copyright of this software please
+// contact ASSIST Software at 3d_support@assist.ro
+//
+
+using RealTimeWeather;
+using RealTimeWeather.Managers;
+using RealTimeWeather.WeatherControllers;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class EnviroDemo : MonoBehaviour
+{
+    [SerializeField] private RealTimeWeatherManager _realTimeWeatherManager;
+
+    private void OnValidate()
+    {
+#if (!ENVIRO_PRESENT)
+        Debug.LogError("Install Enviro");
+#else
+        RealTimeWeatherRelativePath.UpdatePath();
+        if(FindObjectOfType<EnviroModuleController>() == null)
+        {
+            _realTimeWeatherManager.ActivateEnviroSimulation();
+        }
+#endif
+    }
+}
---
+++ Assets/ASSIST Software/Real-TimeWeatherPro/Demo/Expanse Demo Scene/ExpanseDemo.cs
@@ -0,0 +1,32 @@
+﻿//
+// Copyright(c) 2023 Real-Time Weather ASSIST Software
+// https://assist-software.net
+//
+// For information about the licensing and copyright of this software please
+// contact ASSIST Software at 3d_support@assist.ro
+//
+
+using RealTimeWeather;
+using RealTimeWeather.Managers;
+using RealTimeWeather.WeatherControllers;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class ExpanseDemo : MonoBehaviour
+{
+    [SerializeField] private RealTimeWeatherManager _realTimeWeatherManager;
+
+    private void OnValidate()
+    {
+#if (!EXPANSE_PRESENT)
+        Debug.LogError("Install expanse");
+#else
+        RealTimeWeatherRelativePath.UpdatePath();
+        if(FindObjectOfType<ExpanseModuleController>() == null)
+        {
+            _realTimeWeatherManager.ActivateExpanseSimulation();
+        }
+#endif
+    }
+}
